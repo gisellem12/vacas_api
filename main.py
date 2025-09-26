34b39afc5acb1720aeaea594de85ca72cfcce81b
renassd <renasou06@gmail.com>
2025-09-26 02:25:02 -0300

    Configurar host, puerto y verificar OPEN_API_KEY para Railway

--- main.py
+++ main.py
@@ -19,4 +19,5 @@ async def read_root():
     return {"message": "Hello World"}
     
 if __name__ == '__main__':
-    uvicorn.run(app)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
