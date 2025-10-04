764f197a904d2a64c07375a93460cf6c73a5dc5f
gisellem12 <gisellemelgarejo12@gmail.com>
2025-10-03 21:08:08 -0300

    Configurar proyecto para despliegue en Vercel

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,9 +2,10 @@
 const nextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NODE_ENV === 'production'
-      ? process.env.NEXT_PUBLIC_API_URL || 'https://vacas-backend.onrender.com'
+      ? 'https://vacas-backend.onrender.com'
       : 'http://localhost:8000',
   },
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
